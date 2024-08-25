4313fc3dcdc6a2fb4bfa8b3d125b3ffacb9681da
lekoma <marco.leko@jsmd-group.com>
2024-08-25 16:17:26 +0200

    finalize entry page

--- client/App.tsx
+++ client/App.tsx
@@ -6,9 +6,9 @@ import { RootComponentProps } from "../global";
 function App(props: RootComponentProps["rootComponentProps"]): JSX.Element {
   return (
     <header className={styles.header}>
-      <h3>Welcome to the JSMDG's {props.hello} Setup! 🚀</h3>
+      <h3>Welcome to the JSMDG's {props.hello} Setup!</h3>
       <p>
-        Kickstart your next project with our powerful, scalable foundation,
+        Kickstart your next project with our powerful and scalable foundation,
         designed to help you build modern and fast applications effortlessly.
       </p>
       <br />
